eb47614b231c2d4cb1dff14d53727eee39bf8c93
Agata Stawarz <agata.stawarz94@gmail.com>
2019-03-22 23:24:39 +0100

    Add initial project structure

---
+++ script.js
@@ -0,0 +1,29 @@
+var loginElement = document.getElementById("login");
+var btnSearch = document.getElementById("search");
+var listOfRepos = document.getElementById("listOfRepos");
+var newline = "\r\n";
+
+function addRepoToList(repoName) {
+    var li = document.createElement("li");
+    var name = document.createElement("h3");
+    var language = document.createElement("span");
+    var desc = document.createElement("p");
+    name.innerHTML = repoName 
+    li.appendChild(name);
+    li.appendChild(language);
+    li.appendChild(desc);
+    listOfRepos.appendChild(li);
+}
+
+btnSearch.addEventListener("click", function(){
+    fetch("https://api.github.com/users/" + loginElement.value + "/repos")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(repos) {
+        listOfRepos.innerHTML = "";
+        repos.forEach(function(repo) {
+            addRepoToList(repo.name);
+        });
+    });
+}); 
