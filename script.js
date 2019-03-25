7ea744e6ae51b0f7a7d1c409fbeaee24e40e26b7
Agata Stawarz <agata.stawarz94@gmail.com>
2019-03-25 12:17:42 +0100

    Add separate table with list of used programming languages

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ var loginElement = document.getElementById("login");
 var btnSearch = document.getElementById("search");
 var listOfRepos = document.getElementById("listOfRepos");
 var newline = "\r\n";
-var languageCount = {}
+var languageCount = {};
+var listOfLanguages = document.getElementById("listOfLanguages");
 
 function addRepoToList(repoName, repoLanguage, repoDesc) {
     var li = document.createElement("li");
-    var name = document.createElement("h3");
-    var language = document.createElement("span");
-    var desc = document.createElement("p");
-    name.innerHTML = repoName;
-    language.innerHTML = repoLanguage;
-    desc.innerHTML = repoDesc;
-    li.appendChild(name);
-    li.appendChild(language);
-    li.appendChild(desc);
+    var nameHeader = document.createElement("h3");
+    var languageSpan = document.createElement("span");
+    var descParagraph = document.createElement("p");
+    nameHeader.innerHTML = repoName;
+    languageSpan.innerHTML = repoLanguage;
+    descParagraph.innerHTML = repoDesc;
+    li.appendChild(nameHeader);
+    li.appendChild(languageSpan);
+    li.appendChild(descParagraph);
     listOfRepos.appendChild(li);
 }
 
@@ -25,18 +26,38 @@ function countLanguages(language) {
     languageCount[language]++;
 }
 
+function addLanguageToList(languageName, languageCount) {
+    var tr = document.createElement("tr");
+    var languageTd = document.createElement("td");
+    var countTd = document.createElement("td");
+    languageTd.innerHTML = languageName;
+    countTd.innerHTML = languageCount;
+    tr.appendChild(languageTd);
+    tr.appendChild(countTd);
+    listOfLanguages.appendChild(tr);
+}
+
+function reset() {
+    listOfRepos.innerHTML = "";
+    listOfLanguages.innerHTML = "";
+    languageCount = {};
+}
+
 btnSearch.addEventListener("click", function(){
     fetch("https://api.github.com/users/" + loginElement.value + "/repos")
     .then(function(response) {
       return response.json();
     })
     .then(function(repos) {
-        listOfRepos.innerHTML = "";
-        languageCount = {};
+        reset();
         repos.forEach(function(repo) {
             addRepoToList(repo.name, repo.language, repo.description);
             countLanguages(repo.language);
         });
-        console.log(languageCount);
+        let languagesNames = Object.keys(languageCount);
+        languagesNames.forEach(function(language) {
+            console.log(language + " - " + languageCount[language]);
+            addLanguageToList(language, languageCount[language]); 
+        });
     });
 });
