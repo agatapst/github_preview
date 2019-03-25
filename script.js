6eb175fe82c9d9d86bcf79accb17dc2d9f5e957c
Agata Stawarz <agata.stawarz94@gmail.com>
2019-03-25 21:37:11 +0100

    Add bar chart presenting projects with a given programming language

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var loginElement = document.getElementById("login");
 var btnSearch = document.getElementById("search");
 var listOfRepos = document.getElementById("listOfRepos");
+var languagesChartCanvas = document.getElementById('myChart');
 var newline = "\r\n";
 var languageCount = {};
 var listOfLanguages = document.getElementById("listOfLanguages");
+var languagesChart;
 
 function addRepoToList(repoName, repoLanguage, repoDesc) {
     var li = document.createElement("li");
@@ -41,6 +43,50 @@ function reset() {
     listOfRepos.innerHTML = "";
     listOfLanguages.innerHTML = "";
     languageCount = {};
+    if(languagesChart) {
+        languagesChart.destroy();
+    }
+}
+
+function createLanguagesChart() {
+    var ctx = languagesChartCanvas.getContext('2d');
+    languagesChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: Object.keys(languageCount),
+            datasets: [{
+                label: '# of projects with a given programming language',
+                data: Object.values(languageCount),
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 206, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(153, 102, 255)',
+                    'rgb(255, 159, 64)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        stepSize: 1
+                    }
+                }]
+            }
+        }
+    });
 }
 
 btnSearch.addEventListener("click", function(){
@@ -57,7 +103,11 @@ btnSearch.addEventListener("click", function(){
         let languagesNames = Object.keys(languageCount);
         languagesNames.forEach(function(language) {
             console.log(language + " - " + languageCount[language]);
-            addLanguageToList(language, languageCount[language]); 
+            addLanguageToList(language, languageCount[language]);
         });
+        createLanguagesChart()
     });
 });
+
+
+
