3ecc6dfa54d5f36ff1dd8763b33912a47faac6ef
Agata Stawarz <agata.stawarz94@gmail.com>
2019-03-26 20:31:11 +0100

    Code refactoring

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var loginElement = document.getElementById("login");
-var btnSearch = document.getElementById("search");
-var listOfRepos = document.getElementById("list-of-repos");
-var languagesChartCanvas = document.getElementById('my-chart');
-var mainContent = document.getElementById('main-content')
-var message = document.getElementById('message-alert')
-var newline = "\r\n";
-var languageCount = {};
-var listOfLanguages = document.getElementById("list-of-languages");
-var languagesChart;
+const loginElement = document.getElementById("login");
+const btnSearch = document.getElementById("search-button");
+const listOfRepos = document.getElementById("list-of-repos");
+const languagesChartCanvas = document.getElementById("my-chart");
+const mainContent = document.getElementById("main-content");
+const message = document.getElementById("message-alert");
+const listOfLanguages = document.getElementById("list-of-languages");
 
-function addRepoToList(repoName, repoLanguage, repoDesc) {
-    var li = document.createElement("li");
-    var nameHeader = document.createElement("h3");
-    var languageSpan = document.createElement("span");
-    var descParagraph = document.createElement("p");
-    nameHeader.innerHTML = repoName;
-    languageSpan.innerHTML = repoLanguage;
-    descParagraph.innerHTML = repoDesc;
+let languageCount = {};
+let languagesChart;
+
+function addRepoToList(repo) {
+    let li = document.createElement("li");
+    let nameHeader = document.createElement("h3");
+    let languageSpan = document.createElement("span");
+    let descParagraph = document.createElement("p");
+    nameHeader.innerHTML = repo.name;
+    languageSpan.innerHTML = repo.language;
+    descParagraph.innerHTML = repo.description;
     li.appendChild(nameHeader);
     li.appendChild(languageSpan);
     li.appendChild(descParagraph);
@@ -24,16 +24,16 @@ function addRepoToList(repoName, repoLanguage, repoDesc) {
 }
 
 function countLanguages(language) {
-    if(languageCount[language] === undefined) {
+    if(!languageCount[language]) {
         languageCount[language] = 0;
     }
     languageCount[language]++;
 }
 
 function addLanguageToList(languageName, languageCount) {
-    var tr = document.createElement("tr");
-    var languageTd = document.createElement("td");
-    var countTd = document.createElement("td");
+    let tr = document.createElement("tr");
+    let languageTd = document.createElement("td");
+    let countTd = document.createElement("td");
     languageTd.innerHTML = languageName;
     countTd.innerHTML = languageCount;
     tr.appendChild(languageTd);
@@ -48,74 +48,72 @@ function reset() {
     if(languagesChart) {
         languagesChart.destroy();
     }
-    message.classList.add('is-hidden');
+    message.classList.add("is-hidden");
 }
 
 function createLanguagesChart() {
-    var ctx = languagesChartCanvas.getContext('2d');
+    let ctx = languagesChartCanvas.getContext("2d");
     languagesChart = new Chart(ctx, {
-        type: 'pie',
+        type: "pie",
         data: {
             labels: Object.keys(languageCount),
             datasets: [{
-                label: '# of projects with a given programming language',
+                label: "# of projects with a given programming language",
                 data: Object.values(languageCount),
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.5)',
-                    'rgba(54, 162, 235, 0.5)',
-                    'rgba(255, 206, 86, 0.5)',
-                    'rgba(75, 192, 192, 0.5)',
-                    'rgba(153, 102, 255, 0.5)',
-                    'rgba(265, 159, 64, 0.5)',
-                    'rgba(255, 159, 90, 0.5)',
-                    'rgba(255, 11, 64, 0.5)',
-                    'rgba(200, 130, 64, 0.5)',
-                    'rgba(125, 159, 64, 0.5)'
+                    "rgba(255, 99, 132, 0.5)",
+                    "rgba(54, 162, 235, 0.5)",
+                    "rgba(255, 206, 86, 0.5)",
+                    "rgba(75, 192, 192, 0.5)",
+                    "rgba(153, 102, 255, 0.5)",
+                    "rgba(265, 159, 64, 0.5)",
+                    "rgba(255, 159, 90, 0.5)",
+                    "rgba(255, 11, 64, 0.5)",
+                    "rgba(200, 130, 64, 0.5)",
+                    "rgba(125, 159, 64, 0.5)"
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(265, 159, 90, 1)',
-                    'rgba(255, 11, 64, 1)',
-                    'rgba(200, 130, 64, 1)',
-                    'rgba(125, 159, 64, 1)',
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                    "rgba(265, 159, 90, 1)",
+                    "rgba(255, 11, 64, 1)",
+                    "rgba(200, 130, 64, 1)",
+                    "rgba(125, 159, 64, 1)"
                 ],
                 borderWidth: 1
             }]
-        },
+        }
     });
 }
 
-btnSearch.addEventListener("click", function(){
+btnSearch.addEventListener("click", function() {
     fetch("https://api.github.com/users/" + loginElement.value + "/repos")
     .then(function(response) {
         if(response.ok) {
             return response.json();
         }
-        throw new Error('Network response was not ok.');
+        throw new Error("Network response was not ok.");
     })
     .then(function(repos) {
         reset();
-        mainContent.classList.remove('is-hidden');
+        mainContent.classList.remove("is-hidden");
         repos.forEach(function(repo) {
-            addRepoToList(repo.name, repo.language, repo.description);
+            addRepoToList(repo);
             countLanguages(repo.language);
         });
         let languagesNames = Object.keys(languageCount);
         languagesNames.forEach(function(language) {
-            console.log(language + " - " + languageCount[language]);
             addLanguageToList(language, languageCount[language]);
         });
         createLanguagesChart()
     })
     .catch(function(error) {
-        console.log('There has been a problem with your fetch operation: ', error.message);
-        mainContent.classList.add('is-hidden');
-        message.classList.remove('is-hidden')
+        console.log("There has been a problem with your fetch operation: ", error.message);
+        mainContent.classList.add("is-hidden");
+        message.classList.remove("is-hidden")
     })
 });
-
